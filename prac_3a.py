d2e0c8a186edd18ef6afecaa6f410fce8d05e65e
Arxel <1amitpandit2468@gamil.com>
2021-09-23 05:24:10 +0530

    Changed method for prac_3a

--- prac_3a.py
+++ prac_3a.py
@@ -1,20 +1,21 @@
-# def panagram(nt):
-#       check="abcdefghijklmnopqrstuvwxyz"
-#   for l in check:
-#     if(l in nt):
-#          continue
-#     else:
-#          return False
-#    return True           
+def panagram(nt):
+      check="abcdefghijklmnopqrstuvwxyz"
 
-# n=input("Enter Any Text:\n")
-# if(panagram(n.lower())):
-#   print("Yes It is a Pangram")
-# else:
-#   print("No It is not a Pangram")
+      for l in check:
+       if(l in nt):
+         continue
+       else:
+         return False
+      return True           
 
-import string
-def ispangram(str1,alphabet=string.ascii_lowercase):
-    alphaset=set(alphabet)
-    return alphaset<=set(str1.lower())
-print(ispangram("hello world"))
+n=input("Enter Any Text:\n")
+if(panagram(n.lower())):
+  print("Yes It is a Pangram")
+else:
+  print("No It is not a Pangram")
+
+# import string
+# def ispangram(str1,alphabet=string.ascii_lowercase):
+#     alphaset=set(alphabet)
+#     return alphaset<=set(str1.lower())
+# print(ispangram("hello world"))
